b377fb223f3cb46f024a3494c1fea79425ea3598
mohamed naif <naifm371@gmail.com>
2024-03-21 01:10:45 +0200

    Resolved

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -49,10 +49,20 @@ class _SplashScreenState extends State<SplashScreen>
       backgroundColor: const Color.fromARGB(255, 9, 145, 138),
       body: Container(
         decoration: const BoxDecoration(
-            image: DecorationImage(
-          image: AssetImage(
-              'assets/quiz-logo-with-speech-bubble-symbols-concept-of-questionnaire-show-sing-quiz-button-question-competition-png.webp'),
-        )),
+          image: DecorationImage(
+            image: AssetImage(
+                "assets/quiz-seamless-pattern-in-doodle-style-illustration-back-to-school-concept-stationery-symbols-on-a-white-background-pattern-hand-drawn-for-print-and-game-quiz-vector.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Container(
+          decoration: const BoxDecoration(
+              color: Color.fromARGB(172, 7, 27, 44),
+              image: DecorationImage(
+                image: AssetImage(
+                    'assets/quiz-logo-with-speech-bubble-symbols-concept-of-questionnaire-show-sing-quiz-button-question-competition-png.webp'),
+              )),
+        ),
       ),
     );
   }
