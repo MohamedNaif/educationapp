04322c6c522fb1fb6d76720b6946c0fd91719901
AhmedNasser24 <https://github.com/AhmedNasser24>
2024-03-22 06:52:22 +0200

    finish problem with selected answer item

--- lib/features/student/presentation/screens/widgets/answer_list.dart
+++ lib/features/student/presentation/screens/widgets/answer_list.dart
@@ -1,6 +1,8 @@
+import 'package:educationapp/features/student/presentation/manager/quiz_body_cubit/quiz_body_cubit.dart';
 import 'package:educationapp/features/student/presentation/screens/widgets/non_selected_answer_item.dart';
 import 'package:educationapp/features/student/presentation/screens/widgets/selected_answer_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AnswerListWidget extends StatefulWidget {
   const AnswerListWidget(
@@ -14,8 +16,19 @@ class AnswerListWidget extends StatefulWidget {
 
 class _AnswerListWidgetState extends State<AnswerListWidget> {
   int selectedAnswerIndex = -1;
+  int currQuestionIndex = 0 ;
+  void check(){
+    QuizBodyCubit blocProvider = BlocProvider.of<QuizBodyCubit>(context) ;
+    if ( currQuestionIndex != blocProvider.questionIndex){
+      setState(() {
+        selectedAnswerIndex = -1 ;
+        currQuestionIndex = blocProvider.questionIndex ;
+      });
+    }
+  }
   @override
   Widget build(BuildContext context) {
+    check() ;
     return Column(
       children: widget.answerList.asMap().entries.map((e) {
         int index = e.key;
